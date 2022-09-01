b1e034718506a4a292c56ca67a7e9cfb3639af51
Charles B <cberganz@cberganz.42.fr>
2022-09-01 13:33:49 -0400

    project finished

--- srcs/requirements/bonus/backup/tools/backup.sh
+++ srcs/requirements/bonus/backup/tools/backup.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-echo "Backup service started"
-
-while true;
-do
-	time=$(date '+%Y-%m-%d %H:%M:%S')
-	mkdir /bk/"$time"
-	cp -Rf /db/* /bk/"$time"
-	echo "Database backup done !"
-	sleep 3m
-done
+sleep 120
+echo "Backup service started."
+time=$(date '+%Y-%m-%d %H:%M:%S')
+mkdir /bk/"$time"
+cp -Rf /db/* /bk/"$time"/
+echo "Database backup done !"
+echo "Backup service stopped."
