3c8c6b8914927513998f6f70ff26b6eb02c24ece
Charles B <cberganz@cberganz.42.fr>
2022-09-06 13:47:21 +0200

    Setup cron every 10 min instead of every min

--- srcs/requirements/bonus/backup/tools/crontab_set.sh
+++ srcs/requirements/bonus/backup/tools/crontab_set.sh
@@ -2,7 +2,7 @@
 
 if ! crontab -l >/dev/null 2>&1; then
 	echo "Adding cron job..."
-	echo "* * * * * /etc/backup.sh" | crontab -
+	echo "*/10 * * * * /etc/backup.sh" | crontab -
 fi
 
 echo "Backup service started !"
